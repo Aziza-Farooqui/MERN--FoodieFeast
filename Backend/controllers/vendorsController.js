e53c9e9888e3a00d37af4e9defcc81610c067d77
Aziza-Farooqui-au50 <azizafarooqui2@gmail.com>
2024-09-15 19:57:25 +0530

    Vendor controller updated

---
+++ Backend/controllers/vendorsController.js
@@ -0,0 +1,31 @@
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+const Vendor = require('../models/vendors');
+
+const vendorRegister = async(req,res)=>{
+    const {username,email,password}=req.body;
+
+    try{
+        const vendorEmail = new Vendor.findOne({email});
+        if(vendorEmail){
+            return res.status(400).json("Email already exists")
+        }
+
+        const hashedPass = await bcrypt.hash(password, 10);
+
+        const newVendor=new Vendor({
+            username,
+            email,
+            password:hashedPass
+        });
+
+        await newVendor.save();
+        res.status(201).json({message:"Vendor registerd successfully"})
+
+    } catch(error){
+        console.error(error);
+       res.status(500).json({error: "Internal server error"})
+    }
+}     
+
+module.exports={vendorRegister}
